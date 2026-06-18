81d3acf41be0154f5432c3833428612e618bc9c0
Rachael Mirembe <rachaelmirembe55@gmail.com>
2026-06-18 11:04:40 +0300

    Update DashboardHeader

--- src/components/DashboardHeader.tsx
+++ src/components/DashboardHeader.tsx
@@ -1,6 +1,6 @@
 export default function DashboardHeader() {
   return (
-    <div className="bg-white border-2 flex items-center justify-center rounded-2xl border-black shadow-md p-4 inline-block mb-8">
+    <div className=" w-full p-4 flex justify-center "><div className="bg-white border-2 flex items-center justify-center rounded-2xl border-black shadow-md p-4 inline-block mb-8">
       <h1 className="text-4xl font-bold text-purple-700">
         Food Admin Dashboard
       </h1>
@@ -8,6 +8,8 @@ export default function DashboardHeader() {
       <p className="flex center  text-gray-500 mt-2">
         Visual food performance overview
       </p>
+    </div>
+  
     </div>
   );
 }
